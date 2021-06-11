10629ecb58074a7f27af3af521f5944236b5d856
Caitlyn Mills <caitlyn.mills@accenture.com>
2021-06-11 14:31:03 +0930

    Add manage implementation class

---
+++ src/impl/dependency/manageImpl.ts
@@ -0,0 +1,22 @@
+import { core } from "@salesforce/command";
+import { SFPowerkit, LoggerLevel } from "../../sfpowerkit";
+import queryApi from "../../utils/queryExecutor";
+import { chunkArray } from "../../utils/chunkArray";
+import { MetadataSummary } from "../metadata/retriever/metadataSummaryInfoFetcher";
+import { ProgressBar } from "../../ui/progressBar";
+
+export default class manageImpl{
+
+    public bumpMinor(){
+
+    }
+
+    public bumbMajor(){
+
+    }
+
+    public bumpAll(){
+
+    }
+
+}
